1d534f50aba68fbc145d14d7e6f2e7e153fe7de1
Greg <gregcourt10@gmail.com>
2024-06-30 09:24:44 +0100

    changed pip from dynamic to static

--- src/main.tf
+++ src/main.tf
@@ -65,7 +65,7 @@ resource "azurerm_public_ip" "vm" {
   name                = "pip-${var.workload_name}-${var.location_short}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
   sku                 = "Basic"
 }
 
